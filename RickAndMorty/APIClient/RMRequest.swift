f1f4ad98c2906aec30dc5fcfa814fa574bb7e115
Kelvin Ling <kelvinlssiang9393@hotmail.com>
2022-12-25 00:28:45 +0800

    Add docstrings and API related files

---
+++ RickAndMorty/APIClient/RMRequest.swift
@@ -0,0 +1,13 @@
+//
+//  RMRequest.swift
+//  RickAndMorty
+//
+//  Created by KELVIN LING SHENG SIANG on 25/12/2022.
+//
+
+import Foundation
+
+/// Object that represents a single API call
+final class RMRequest {
+    
+}
